66f61a39120ca59f0d07a0bfc7ce3b131f713705
WaruiAlfred <alfredwarui17@gmail.com>
2023-06-22 22:20:45 +0300

    update Link component to render vodes and author

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,18 +1,49 @@
 import { FC } from "react";
+import { AUTH_TOKEN } from "../constants";
+import { timeDifferenceForDate } from "../utils";
 
 interface LinkProps {
   link: {
-    description: string;
+    createdAt: string;
     url: string;
+    description: string;
+    postedBy: {
+      name: string;
+    };
+    votes: any;
   };
+  index: number;
 }
 
-const Link: FC<LinkProps> = ({ link }) => {
-  // const { link } = props;
+const Link: FC<LinkProps> = ({ link, index }) => {
+  const authToken = localStorage.getItem(AUTH_TOKEN);
   return (
-    <div>
-      <div>
-        {link.description} ({link.url})
+    <div className="flex mt2 items-start">
+      <div className="flex items-center">
+        <span className="gray">{index + 1}.</span>
+        {authToken && (
+          <div
+            className="ml1 gray f11"
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              console.log("Clicked vote button");
+            }}
+          >
+            ▲
+          </div>
+        )}
+      </div>
+      <div className="ml1">
+        <div>
+          {link.description} ({link.url})
+        </div>
+        {
+          <div className="f6 lh-copy gray">
+            {link.votes.length} votes | by{" "}
+            {link.postedBy ? link.postedBy.name : "Unknown"}{" "}
+            {timeDifferenceForDate(link.createdAt)}
+          </div>
+        }
       </div>
     </div>
   );
